8253b64703f3f0d897af5da8a0871678069c0031
Sean Cheng <seanczw@hotmail.com>
2024-09-14 19:19:58 +0800

    Added webserver cookbook (along with initial tests)

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
+
+# recipes/default.rb
+
+# Install Apache
+package 'apache2' do
+    action :install
+end
+
+# Start Apache
+service 'apache2' do
+    action [:enable, :start]
+end
+
+# Create index file
+file '/var/www/html/index.html' do
+    content '<html>Welcome to the OSU Open Source Lab!</html>'
+    action :create
+end
--- cookbooks/webserver/test/integration/default/default_test.rb
+++ cookbooks/webserver/test/integration/default/default_test.rb
@@ -3,14 +3,12 @@
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe service('apache2.service') do
+  it { should be_enabled }
+  it { should be_running }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe http('http://localhost') do
+  its('status') { should cmp 200 }
+  its('body') { should match /Welcome to the OSU Open Source Lab!/ }
 end
